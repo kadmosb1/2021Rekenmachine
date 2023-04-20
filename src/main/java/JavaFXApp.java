1551bdecb1a4eaacc85a2c2dfedf07a762ab6d93
Lossttt <lfatmi.ayoub@gmail.com>
2023-04-20 20:47:28 +0200

    Added compute addition

--- src/main/java/JavaFXApp.java
+++ src/main/java/JavaFXApp.java
@@ -29,8 +29,8 @@ public class JavaFXApp extends Application {
         return Integer.parseInt (textField.getText ());
     }
 
-    protected int computeAdd (int number1, int number2) {
-        return 0;
+    protected int computeAdd(int number1, int number2) {
+            return number1 + number2;
     }
 
     protected int computeMultiply (int number1, int number2) {
--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -2,8 +2,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
+
+    private final JavaFXApp calculator = new JavaFXApp();
+
     @Test
     void aDummyTest() {
         assertEquals(0,0);
+    }
+
+    @Test
+    void testAdditionWithPositiveNumbers() {
+        int result = calculator.computeAdd(2, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void testAdditionWithNegativeNumbers() {
+        int result = calculator.computeAdd(-2, -3);
+        assertEquals(-5, result);
+    }
+
+    @Test
+    void testAdditionWithZero() {
+        int result = calculator.computeAdd(0, 5);
+        assertEquals(5, result);
     }
 }
